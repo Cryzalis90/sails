9990197f0f7c0a08e61d7ce57ff49f78340c1e2c
dmitry.maslo <dmitriy.maslo@itrexgroup.com>
2017-01-15 11:19:36 +0200

    relationships

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,8 +6,58 @@
  */
 
 module.exports = {
+  index:function(req,res,next){
+    Customer.find(req.param('id'),function foundCustomer(err,customers){
+      if(err) return next(err);
+
+      return res.view({
+        customers:customers
+      });
+    });
+  },
   'new':function (req,res) {
     res.view();
+  },
+
+  create:function(req,res,next){
+    Customer.create(req.params.all(),function customerCreated(err,customer){
+      if(err) return next(err);
+      res.redirect('/customer/show/' + customer.id);
+    });
+  },
+
+  show:function(req,res,next){
+    Customer.findOne(req.param('id'),function foundCustomer(err,customer){
+
+      if(err) return next(err);
+      if(!customer) return next();
+      return res.view({
+        customer:customer
+      });
+    });
+  },
+  edit:function(req,res,next){
+    Customer.findOne(req.param('id'),function foundCustomer(err,customer){
+
+      if(err) return next(err);
+      if(!customer) return next();
+      return res.view({
+        customer:customer
+      });
+    });
+  },
+  update:function(req,res,next){
+    Customer.update(req.param('id'),req.params.all(),function customerUpdate(err){
+      if(err) {
+        return res.redirect('customer/edit'+req.param('id'));
+      }
+      res.redirect('/customer/show/' + req.param('id'));
+    });
+  },
+  destroy:function(req,res,next){
+    Customer.destroy(req.param('id')).exec(function(){
+      res.redirect('/customer/');
+    });
   }
 
 };
